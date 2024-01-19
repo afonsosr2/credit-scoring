261f3f9912da302ac7d52b2b9b00ca52aaa7840a
afonsosr2 <afonsosr2@gmail.com>
2024-01-19 11:34:27 -0300

    atualizando app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-# dados = pd.read_csv("df_clean")
+dados = pd.read_csv("https://raw.githubusercontent.com/afonsosr2/credit-scoring/main/df_clean.csv")
 st.write("# Simulador de Avaliação de Crédito")
 
